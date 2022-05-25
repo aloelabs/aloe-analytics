02e5f8c9c01e25ee59349902310427652440f63e
Matt Evenson <mevenson@usc.edu>
2022-05-25 00:16:23 -0700

    have a bit more fun

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -4,4 +4,7 @@ scriptPath=$(dirname "$(readlink -f "$0")")
 source "${scriptPath}/.env.sh"
 
 cd /project && \
-    meltano elt tap-ccxt target-postgres --job_id=ccxt-to-postgres
+    meltano elt tap-ccxt target-postgres --job_id=ccxt-to-postgres &
+
+cd /project && \
+    meltano elt tap-ethereum target-postgres --job_id=ethereum-to-postgres &
