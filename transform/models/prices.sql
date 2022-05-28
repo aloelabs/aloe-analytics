daacbb1c8f5e0fc3d5f669b33f0cb28aab7971e2
Matt Evenson <mevenson@usc.edu>
2022-05-27 21:28:54 -0700

    add prices table

---
+++ transform/models/prices.sql
@@ -0,0 +1,15 @@
+SELECT
+    (
+        tsrange(
+            ohlcv.timestamp,
+            ohlcv.timestamp + INTERVAL '1 minute'
+        )
+    ) AS "interval",
+    ohlcv.exchange,
+    ohlcv.base,
+    ohlcv.quote,
+    (
+        ohlcv.open + ohlcv.close
+    ) / 2 AS price
+FROM
+    tap_ccxt.ohlcv
