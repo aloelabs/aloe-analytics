86c8200756caedd5f2e1d7a4c3d52617fd324c2a
Matt Evenson <mevenson@usc.edu>
2022-05-28 16:23:32 -0700

    convert blocks to incremental

--- transform/models/blocks.sql
+++ transform/models/blocks.sql
@@ -1,3 +1,9 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = 'block_number',
+    indexes = [ {'columns': ['block_number'] },{ 'columns': ['timestamp'] }]
+) }}
+
 SELECT
     TO_TIMESTAMP(CAST("timestamp" AS bigint)) AS TIMESTAMP,
     CAST(
@@ -5,3 +11,15 @@ SELECT
     ) AS block_number
 FROM
     tap_thegraph.mainnet_block -- TODO: fix the replication key for tap-thegraph (it always starts from the beginning)
+
+{% if is_incremental() %}
+WHERE
+    CAST(
+        "id" AS bigint
+    ) > (
+        SELECT
+            MAX(block_number)
+        FROM
+            {{ this }}
+    )
+{% endif %}
