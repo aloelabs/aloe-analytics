12a5de9dd16baeba516ec5abe2bdef20d7dadfc4
Matt Evenson <mevenson@usc.edu>
2022-05-27 23:12:20 -0700

    progress

--- transform/models/pool_returns.sql
+++ transform/models/pool_returns.sql
@@ -1,5 +1,16 @@
 -- Create a mapping from pool address to token
+-- At every block, what was the price (so match timestamp and calculate holdings)
+-- Should I do a SQL course or something?
+WITH observations_with_timestamps AS(
+    SELECT
+        *
+    FROM
+        {{ ref("aloe_blend") }}
+        JOIN {{ ref("blocks") }} USING (block_number)
+)
 SELECT
     *
 FROM
-    {{ ref("aloe_blend") }}
+    observations_with_timestamps
+    JOIN {{ ref("prices") }}
+    ON "timestamp" :: TIMESTAMP <@ "interval"
