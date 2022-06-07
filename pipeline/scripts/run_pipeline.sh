54559ef96d25412ff3b8e4c62c1002f8e4612f38
Matt Evenson <mevenson@usc.edu>
2022-06-07 10:47:46 -0700

    work on docker compose

--- pipeline/scripts/run_pipeline.sh
+++ pipeline/scripts/run_pipeline.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+# Load environment variables
 scriptPath=$(dirname "$(readlink -f "$0")")
 source "${scriptPath}/.env.sh"
 
+# Run pipelines
 meltano run tap-ccxt target-postgres &
 meltano run tap-ethereum target-postgres &
 meltano run tap-thegraph target-postgres &
 
-wait
+# Exit if any of the pipelines had errors
+wait || exit $?
 
+# Run dbt transforms
 meltano run dbt-postgres:seed dbt-postgres:run 
--- pipeline/transform/models/global_stats.sql
+++ pipeline/transform/models/global_stats.sql
@@ -1,3 +1,7 @@
+{{ config(
+    materialized = 'table'
+) }}
+
 SELECT
     (
         SELECT
