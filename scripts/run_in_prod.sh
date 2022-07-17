d950068c25ad1bd77d104be6df5b22a77af82a6e
Matt Evenson <mevenson@usc.edu>
2022-07-17 16:49:26 -0700

    typo ifx

--- scripts/run_in_prod.sh
+++ scripts/run_in_prod.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker compose -f production.yml pull down && 
+docker compose -f production.yml down && 
 docker system prune -a && 
 docker compose -f production.yml pull && 
 docker compose -f production.yml up -d
