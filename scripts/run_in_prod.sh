974ae67286eec544a6786c73040e5584b019b124
Matt Evenson <mevenson@usc.edu>
2022-07-17 17:03:26 -0700

    improve the "deployment" script

--- scripts/run_in_prod.sh
+++ scripts/run_in_prod.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
-docker compose -f production.yml down && 
-docker system prune -a && 
-docker compose -f production.yml pull && 
+# Kill existing tmux server
+tmux kill-server
+
+# Stop the running services
+docker compose -f production.yml down
+
+# Remove the old Docker images and containers
+docker system prune -a
+
+# Pull the latest images
+docker compose -f production.yml pull
+
+# Start the containers
 docker compose -f production.yml up -d
+
+# View the logs for the pipeline (left) and api (right)
+tmux new-session -d -s aloe_analytics 'docker compose -f production.yml logs -f pipeline'
+tmux split-window -h 'docker compose -f production.yml logs -f api'
+tmux attach -t aloe_analytics
